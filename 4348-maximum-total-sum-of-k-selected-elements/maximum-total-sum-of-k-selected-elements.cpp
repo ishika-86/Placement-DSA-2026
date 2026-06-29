1bd35a7f3aa7e2d748f4a86fe2254d5041851e3b
ishika-86 <ishikabansal523@gmail.com>
2026-06-29 10:15:33 +0530

    Time: 69 ms (70.00%) | Memory: 186.2 MB (80.00%) - LeetSync

---
+++ 4348-maximum-total-sum-of-k-selected-elements/maximum-total-sum-of-k-selected-elements.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long maxSum(vector<int>& nums, int k, int mul) {
+        sort(nums.begin(),nums.end(), greater<int>());
+        int n = nums.size();
+
+        long long sum = 0;
+        for(int i=0; i<k; i++){
+            if (mul>0) {
+                sum += (long long)nums[i] * mul;
+                mul--;
+            }
+            else sum += nums[i];   
+        } 
+
+        return sum;
+        
+    }
+};
