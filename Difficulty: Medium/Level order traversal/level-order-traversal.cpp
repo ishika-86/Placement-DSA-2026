c917714759b5839daa19514853375272640dab40
ishika-86 <ishikabansal523@gmail.com>
2026-07-04 12:06:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Level order traversal/level-order-traversal.cpp
@@ -0,0 +1,39 @@
+/* A binary tree Node
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    // Constructor
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    vector<int> levelOrder(Node *root) {
+        // code here
+        vector <int> ans;
+        if(root==nullptr) return ans;
+        queue <Node*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            Node *curr= q.front();
+            q.pop();
+            
+            ans.push_back(curr->data);
+            
+            if(curr->left!=nullptr) q.push(curr->left);
+            if(curr->right!=nullptr) q.push(curr->right);
+            
+        }
+        return ans;
+        
+    }
+};
