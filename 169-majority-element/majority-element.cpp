514a56008a9d347ccc422c73a0f3941893fb90df
ishika-86 <ishikabansal523@gmail.com>
2026-07-06 22:35:22 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (32.13%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        int c = 0;
+        int cand=0;
+        for (int i=0; i<nums.size(); i++){
+            if (c==0) cand=nums[i];
 
-        //Moore's Voting Algorithm
-        int n=nums.size();
-        unordered_map <int,int> mp;
-        for (int i: nums){
-            mp[i]++;
-            if (mp[i]>n/2) return i;
-
+            if (cand==nums[i]) c++;
+            else c--;
         }
-        return -1;
+        return cand;
     }
 };
