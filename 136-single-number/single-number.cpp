cddce719fb2df8b6affad74fe29751026fc37fcd
ishika-86 <ishikabansal523@gmail.com>
2026-07-09 21:07:04 +0530

    Time: 12 ms (11.66%) | Memory: 20.7 MB (58.74%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -2,10 +2,12 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         sort(nums.begin(),nums.end());
+        // Check elements in pairs
         for(int i=0;i<nums.size()-1; i+=2){
             if(nums[i]!=nums[i+1]) return nums[i];
         }
-
+        // If every previous pair matched,
+        // the last element is the answer
         return nums.back();
     }
 };
