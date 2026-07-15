eabee02f4cf3a4e91d952e7c88705bddc4677a24
ishika-86 <ishikabansal523@gmail.com>
2026-07-15 12:17:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum Of Digits/sum-of-digits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    int sumOfDigits(int n) {
+        int sum = 0;
+        while(n!=0){
+            int rev = n%10;
+            sum += rev;
+            n/=10;
+        }
+        return sum;
+    }
+};
