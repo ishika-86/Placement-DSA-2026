96b391acaa5db3e38a7fc47dfd2dfc00bb635524
ishika-86 <ishikabansal523@gmail.com>
2026-07-16 00:24:52 +0530

    Time: 3 ms (28.77%) | Memory: 9.8 MB (78.16%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string clean = "";
-        for (int i=0; i<s.size(); i++)
-            if(isalnum(s[i])) clean += tolower(s[i]);
-
-        string rev = clean; // copy clean to rev
-        reverse(rev.begin(), rev.end());
-        return clean==rev;
+        int i = 0, j = s.size() - 1;
+        while (i < j) {
+            if (!isalnum(s[i])) ++i;
+            else if (!isalnum(s[j])) --j;
+            else if (tolower(s[i]) != tolower(s[j])) return false;
+            else  {++i; --j;}
+            
+        }
+        return true;
     }
 };
