b0365d3405fa6ac8b21109f3e9db62b0c1e1b42b
ishika-86 <ishikabansal523@gmail.com>
2026-07-17 12:23:56 +0530

    Add Wrong Subtraction [A] from Codeforces

---
+++ 977/A - Wrong Subtraction/solution.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+ 
+    int n, k; cin >> n >> k;
+    for(int i = 0; i < k; i++){
+        int rem = n%10;
+        if(rem == 0){
+            n/=10;
+        }
+        else{
+            n--;
+        }
+    }
+ 
+    cout << n << endl;
+ 
+    return 0;
+}
