3020a07fd0824e9f4b5486299a98f5d34b2dc762
ishika-86 <ishikabansal523@gmail.com>
2026-07-17 12:25:41 +0530

    Add RemovevomeR [C] from Codeforces

---
+++ 2241/C - RemovevomeR/solution.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    
+    int t; cin >> t;
+ 
+    while (t--) {
+        int n; string s; cin >> n >> s;
+        int ans = 0;
+ 
+        for (int i = 0; i < n-1; i++) {
+            if (s[i] != s[i+1]) ans++;
+        }
+        cout << (ans==1 ? 2:1) << "
+";
+    }
+    return 0;
+}
