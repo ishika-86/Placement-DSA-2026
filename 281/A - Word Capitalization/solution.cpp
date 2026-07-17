ee672e71934ebe18539fc4555d03277eae9facff
ishika-86 <ishikabansal523@gmail.com>
2026-07-17 12:22:47 +0530

    Add Word Capitalization [A] from Codeforces

---
+++ 281/A - Word Capitalization/solution.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+ 
+    string s;
+    cin>>s;
+    
+    if (s[0] >= 97 && s[0] <= 122)
+        cout << char(s[0]-32);
+    else
+        cout << char(s[0]);
+ 
+    for (int i = 1; i < s.length(); i++){
+        cout << s[i];
+    }
+    
+ 
+    return 0;
+}
