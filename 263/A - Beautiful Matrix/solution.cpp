9df416bfcd0305d90c1f866bb6d20a390e601699
ishika-86 <ishikabansal523@gmail.com>
2026-07-17 12:22:35 +0530

    Add Beautiful Matrix [A] from Codeforces

---
+++ 263/A - Beautiful Matrix/solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+ 
+    int a[5][5];
+    int row=0, col=0;
+    for(int i=0; i<5; i++){
+        for (int j = 0; j < 5; j++){
+            cin>>a[i][j];
+            if(a[i][j]==1){
+                row=i;
+                col=j;
+            }
+        }
+    }
+    int moves = abs(row-2) + abs(col-2);
+    cout<<moves<<endl;    
+ 
+    return 0;
+}
