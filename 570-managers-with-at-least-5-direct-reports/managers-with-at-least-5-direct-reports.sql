3c7cec691db0556f3a37585306188d8bfa51131d
ishika-86 <ishikabansal523@gmail.com>
2026-07-19 13:03:21 +0530

    Time: 318 ms (98.46%) | Memory: 0B (100.00%) - LeetSync

---
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+SELECT name 
+FROM Employee 
+WHERE id IN
+  (SELECT managerId 
+   FROM Employee 
+   GROUP BY managerId 
+   HAVING COUNT(*) >= 5
+  )
+
+
+/* SELECT a.name
+FROM Employee a
+JOIN Employee b
+WHERE a.id = b.managerId
+GROUP BY b.managerId
+HAVING COUNT(*) >= 5 */
