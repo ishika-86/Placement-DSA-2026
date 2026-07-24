f890e2d50fc7469ec7810674908cd8b386780fb2
ishika-86 <ishikabansal523@gmail.com>
2026-07-24 11:49:46 +0530

    Time: 2 ms (77.96%) | Memory: 7.8 MB (38.83%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -3,9 +3,9 @@ public:
 
     int solve(int n, int a, int b){
         if (n==0) return a;
-
         return solve(n-1,b,a+b);
     }
+    
     int fib(int n) {
         return solve(n, 0, 1);
     }
