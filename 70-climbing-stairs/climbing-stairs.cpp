939e5759535875fb00314751b44329c8d5d7fa85
ishika-86 <ishikabansal523@gmail.com>
2026-07-24 12:44:57 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (74.11%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,10 +1,12 @@
+using ll =long long;
 class Solution {
 public:
+
+    int solve(int n, ll a, ll b){
+        if(n==0) return a;
+        return solve(n-1, b, a+b);
+    }
     int climbStairs(int n) {
-        long long a=1, b=1;
-        while(n--){
-            b+=exchange(a,b);
-        }
-        return a;
+        return solve(n,1,1);
     }
 };
