76ea66108a324acb1afd0725fdab2d496217cbaf
ishika-86 <ishikabansal523@gmail.com>
2026-07-30 14:02:09 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (11.60%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,21 +1,10 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-
-        if (s.length() != t.length())
-            return false;
-
-        int freq[26] = {};
-
-        for (int i = 0; i < s.length(); i++) {
-            freq[s[i] - 'a']++;
-            freq[t[i] - 'a']--;
-        }
-
-        for (int x : freq)
-            if (x)
-                return false;
-
+        if (s.length() != t.length()) return false; 
+        unordered_map<char,int> mp;
+        for(char c: s) mp[c]++;
+        for(char c: t) if(--mp[c]<0) return false;
         return true;
     }
 };
