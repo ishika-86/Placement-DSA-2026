589f4fbcf7de8953fa4b0e33a7c583c91cf692b5
ishika-86 <ishikabansal523@gmail.com>
2026-07-31 02:35:06 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (21.63%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.cpp
+++ 414-third-maximum-number/third-maximum-number.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        unordered_set<int>s(nums.begin(), nums.end());
-        priority_queue<int> pq;
-        for(auto i: s) pq.push(i);
-        if(pq.size()<3) return pq.top();
-        pq.pop(); pq.pop();
-        return pq.top();
+        unordered_set<int>st(nums.begin(), nums.end());
+        vector<int>vec(st.begin(),st.end());
+        ranges::sort(vec,greater<int>());
+        return vec.size()>=3? vec[2]: vec[0];
     }
 };
