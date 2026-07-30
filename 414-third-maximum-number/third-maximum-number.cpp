0a953b8d6d3d7010404320d7bfbd4958e56b9296
ishika-86 <ishikabansal523@gmail.com>
2026-07-31 02:30:55 +0530

    Time: 0 ms (100.00%) | Memory: 14.6 MB (7.45%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.cpp
+++ 414-third-maximum-number/third-maximum-number.cpp
@@ -5,7 +5,6 @@ public:
         priority_queue<int> pq;
         for(auto i: s) pq.push(i);
         if(pq.size()<3) return pq.top();
-        int t=2;
         pq.pop(); pq.pop();
         return pq.top();
     }
