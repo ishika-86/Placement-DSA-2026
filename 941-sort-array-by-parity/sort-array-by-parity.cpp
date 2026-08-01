b03ca0e1ef5235e905eeada3f6f2d54202ebb193
ishika-86 <ishikabansal523@gmail.com>
2026-08-02 01:08:09 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (54.16%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.cpp
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -3,8 +3,8 @@ public:
     vector<int> sortArrayByParity(vector<int>& nums) {
         int l=0, r= nums.size()-1;
         while(l<r){
-            if(l<r && (nums[l]&1)==0) l++; //l is even thus correct place
-            else if(l<r && (nums[r]&1)!=0) r--; //r is odd thus correct place
+            if((nums[l]&1)==0) l++; //l is even thus correct place
+            else if((nums[r]&1)!=0) r--; //r is odd thus correct place
             else{
                 swap(nums[l],nums[r]);
                 l++; r--;
