a9a847eeb9ebc5ee0e2335f645e2d5d16bcc65d1
ishika-86 <ishikabansal523@gmail.com>
2026-08-07 08:54:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (93.60%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        long long N=n;
-        bool neg = false;
-        if (N<0){
-            neg=true;
-            N=-N;
+        
+        if(n < 0) {
+            x = 1 / x;
+        } 
+        
+        long num = labs(n);
+        //converts n to a safe long to avoid integer overflow with INT_MIN
+        double pow = 1;
+        
+        while(num){ // equivalent to while(num != 0)
+            if(num & 1) { //even-> no multiply , odd-> multiply
+                pow *= x;
+            }           
+            x *= x;
+            num >>= 1;
         }
-        double ans = 1;
-        while(N>0){
-            if (N&1) ans *= x;
-            x*=x; N>>=1;
-        }
-        return neg? 1.0/ans : ans;
+        
+        return pow;
     }
 };
