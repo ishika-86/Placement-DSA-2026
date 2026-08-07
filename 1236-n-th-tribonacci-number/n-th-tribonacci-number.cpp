82edd752d6b0bdf7f8e76eeec47e15e8efcd7ffc
ishika-86 <ishikabansal523@gmail.com>
2026-08-07 09:25:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (24.35%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -3,12 +3,11 @@ public:
     int tribonacci(int n) {
         if(n==0) return 0;
         else if(n==1||n==2) return 1;
-        int a=0, b=1,c=1;
-        int next;
+        vector <int> dp(n+1);
+        dp[1]=1; dp[2]=1;
         for(int i=3; i<=n; i++){
-            next=a+b+c;
-            a=b; b=c; c=next;
+            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
         }
-        return next;
+        return dp[n];
     }
 };
