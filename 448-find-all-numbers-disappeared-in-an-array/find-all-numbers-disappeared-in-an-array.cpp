f635127d7d9a4a5784f70e29bbd8841f5dc495ce
ishika-86 <ishikabansal523@gmail.com>
2026-08-22 00:11:15 +0530

    Time: 33 ms (31.37%) | Memory: 53.2 MB (56.64%) - LeetSync

---
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int> ans;
+        ranges::sort(nums);
+        for(int i=1; i<=nums.size(); i++){
+            if(!binary_search(nums.begin(), nums.end(), i)) ans.push_back(i);
+        }
+        return ans;
+    }
+};
